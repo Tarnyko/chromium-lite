b6a9a1e48b3bdb320cb382c09ed6a79dd68f0ef5
Manuel Bachmann <mbc@iot.bzh>
2016-02-03 17:35:31 +0000

    Finalize Skia support
    Produced libskia.so is not fully functional with C example.
    
    Signed-off-by: Manuel Bachmann <mbc@iot.bzh>

--- skia/Toolchain-x86_64.cmake
+++ skia/Toolchain-x86_64.cmake
@@ -1,2 +1,4 @@
 ADD_DEFINITIONS(-m64 -msse2 -mfpmath=sse)
 SET(CMAKE_SHARED_LINKER_FLAGS "-m64")
+
+SET(TOOLCHAIN_OPTS_SOURCE_FILES src/opts/SkBitmapFilter_opts_SSE2.cpp src/opts/SkBitmapProcState_opts_SSE2.cpp src/opts/SkBlitRow_opts_SSE2.cpp src/opts/SkBitmapProcState_opts_SSSE3.cpp src/opts/SkOpts_ssse3.cpp src/opts/SkBlitRow_opts_SSE4.cpp src/opts/SkOpts_sse41.cpp src/opts/SkOpts_avx.cpp src/opts/opts_check_x86.cpp)
