56c63380f052b760fa12104b55ffcfb79b744330
Manuel Bachmann <mbc@iot.bzh>
2016-02-08 13:00:39 +0000

    Improve Media support, UI Gfx
    "libmedia.so" now compiles without errors, we update dependencies
    for a better result.
    We still need yasm to assemble platform-specific .asm files.
    
    UI Gfx now builds correctly under Ozone.
    
    Signed-off-by: Manuel Bachmann <mbc@iot.bzh>

---
+++ 05a-media/Toolchain-x86_64.cmake
@@ -0,0 +1,1 @@
+SET(TOOLCHAIN_OPTS_SOURCE_FILES base/simd/convert_rgb_to_yuv_sse2.cc base/simd/convert_rgb_to_yuv_ssse3.cc base/simd/convert_yuv_to_rgb_x86.cc base/simd/filter_yuv_sse2.cc)
---
+++ z_examples/media/chromium_media-1_audio_config.cc
@@ -0,0 +1,25 @@
+//  (C)2016 Tarnyko <tarnyko@tarnyko.net>
+// compile with : g++ ... -DUSE_AURA -DUSE_OZONE/-DUSE_X11 -I/usr/include/chromium /usr/lib/chromium/lib/libbase.so /usr/lib/chromium/lib/libskia.so /usr/lib/chromium/lib/liburl.so /usr/lib/chromium/lib/libcrypto.so /usr/lib/chromium/lib/libgpu_command_buffer.so /usr/lib/chromium/lib/libui_gfx.so /usr/lib/chromium/lib/libui_events.so
+
+#include <iostream>				// for << >>
+
+#include "media/base/audio_hardware_config.h"	//  for media::AudioHardwareConfig
+#include "media/audio/audio_parameters.h"	//  for media::AudioParameters
+
+
+ // we create an audio configuration 
+
+int main (int argc, char *argv[])
+{
+	 // Parameters : type, channels, sample rate, bpp, buffer size
+
+	media::AudioParameters input_params (media::AudioParameters::AUDIO_PCM_LOW_LATENCY, media::CHANNEL_LAYOUT_MONO, 44100, 16, 2048);
+	media::AudioParameters output_params (media::AudioParameters::AUDIO_PCM_LOW_LATENCY, media::CHANNEL_LAYOUT_STEREO, 48000, 16, 2048);
+
+	media::AudioHardwareConfig config (input_params, output_params);
+
+	std::cout << "Output sample rate : " << config.GetOutputSampleRate () << std::endl;
+
+	return 0;
+}
+
