b246e32cbe7dbc9fe89916dfbc4ccccb6dbbaf02
Manuel Bachmann <mbc@iot.bzh>
2016-02-09 04:23:17 +0000

    Add CC support
    "libcc.so" fully works !
    
    Signed-off-by: Manuel Bachmann <mbc@iot.bzh>

---
+++ 08a-cc/Toolchain-arm.cmake
@@ -0,0 +1,4 @@
+# This is a stub, ARM does not require anything
+
+# Useless, but this variable cannot be empty
+SET(TOOLCHAIN_OPTS_SOURCE_FILES base/cc_export.h)
---
+++ 08a-cc/Toolchain-x86_64.cmake
@@ -0,0 +1,1 @@
+SET(TOOLCHAIN_OPTS_SOURCE_FILES raster/texture_compressor_etc1_sse.cc)
