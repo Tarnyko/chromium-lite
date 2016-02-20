9198937c0dd1c1fd3056dc5909ab4b6d824ed7cf
Manuel Bachmann <mbc@iot.bzh>
2016-02-20 21:42:50 +0000

    Improve Blink support
    We now build the whole "core" section (static
    & generated source files) and fix missing
    sources.
    
    The "modules" section is on its way...
    
    Signed-off-by: Manuel Bachmann <mbc@iot.bzh>

--- 12a-blink/Toolchain-x86_64.cmake
+++ 12a-blink/Toolchain-x86_64.cmake
@@ -2,7 +2,14 @@ SET(QCMS_DIR "${CMAKE_SOURCE_DIR}/../../../third_party/qcms")
 
 ADD_DEFINITIONS(-DSSE2_ENABLE)
 
-SET(TOOLCHAIN_OPTS_SOURCE_FILES ${QCMS_DIR}/src/transform-sse2.c)
 
-# Generate "platform/heap/asm/SaveRegisters_x86.asm" !
+# GENERATING ASSEMBLY CODE
 
+MESSAGE(STATUS "Generating .o from .asm...")
+
+EXECUTE_PROCESS(COMMAND mkdir -p platform/heap/ WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+EXECUTE_PROCESS(COMMAND yasm -I.. -felf64 -m amd64 -DPIC -DX64POSIX=1 -DPREFIX -o ${CMAKE_BINARY_DIR}/platform/heap/SaveRegisters_x86.o platform/heap/asm/SaveRegisters_x86.asm WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+
+
+SET(TOOLCHAIN_OPTS_SOURCE_FILES ${QCMS_DIR}/src/transform-sse2.c ${CMAKE_BINARY_DIR}/platform/heap/SaveRegisters_x86.o)
