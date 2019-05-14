ee425df0b7fe593fc5f43823e4e08d8c82469e29
Tarnyko <tarnyko@tarnyko.net>
2019-05-14 15:59:01 +0200

    Skia: upgrade to Chrome 58
    Signed-off-by: Tarnyko <tarnyko@tarnyko.net>

--- 05b-skia/Toolchain-arm.cmake
+++ 05b-skia/Toolchain-arm.cmake
@@ -1,4 +1,4 @@
 ADD_DEFINITIONS(-march=armv7-a -mthumb)
 SET(CMAKE_SHARED_LINKER_FLAGS "-march=armv7-a")
 
-SET(TOOLCHAIN_OPTS_SOURCE_FILES src/opts/SkBitmapProcState_opts_arm.cpp src/opts/SkBlitMask_opts_arm.cpp src/opts/SkBlitRow_opts_arm.cpp)
+SET(TOOLCHAIN_OPTS_SOURCE_FILES src/opts/SkBlitMask_opts_arm.cpp src/opts/SkBlitRow_opts_arm.cpp)
