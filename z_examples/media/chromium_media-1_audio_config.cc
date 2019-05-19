70ba7a9288165bb57f7f4bc64f97425d14d5eedc
Tarnyko <tarnyko@tarnyko.net>
2019-05-19 05:04:46 +0200

    Media: upgrade to Chrome 58 (part 2)
    Media: force-enable PPAPI without requiring it.
    Mojo, UI Gfx: enrich for Media.
    Native Client, PPAPI: initial implementation.
    
    Signed-off-by: Tarnyko <tarnyko@tarnyko.net>

--- z_examples/media/chromium_media-1_audio_config.cc
+++ z_examples/media/chromium_media-1_audio_config.cc
@@ -1,24 +1,33 @@
 //  (C)2016 Tarnyko <tarnyko@tarnyko.net>
 // compile with : g++ ... -DUSE_AURA -DUSE_OZONE/-DUSE_X11 -I/usr/include/chromium /usr/lib/chromium/lib/libbase.so /usr/lib/chromium/lib/libskia.so /usr/lib/chromium/lib/liburl.so /usr/lib/chromium/lib/libcrypto.so /usr/lib/chromium/lib/libgpu_command_buffer.so /usr/lib/chromium/lib/libui_gfx.so /usr/lib/chromium/lib/libui_events.so /usr/lib/chromium/lib/libmedia.so
 
-#include <iostream>				// for << >>
+#include <iostream>					// for << >>
 
-#include "media/base/audio_hardware_config.h"	//  for media::AudioHardwareConfig
-#include "media/audio/audio_parameters.h"	//  for media::AudioParameters
+#include "media/audio/audio_manager.h"			// for media::ScopedAudioManagerPtr
+#include "media/audio/alsa/audio_manager_alsa.h"	// for media::AudioManagerAlsa
+#include "media/audio/audio_device_description.h"	// for media::AudioDeviceDescriptions
 
 
  // we create an audio configuration 
 
 int main (int argc, char *argv[])
 {
-	 // Parameters : type, channels, sample rate, bpp, buffer size
+	media::ScopedAudioManagerPtr audio_manager;
+	audio_manager.reset ();
+	//audio_manager = media::TestAudioManagerFactory<AudioManagerAlsa>::Create(&fake_audio_log_factory_);
 
-	media::AudioParameters input_params (media::AudioParameters::AUDIO_PCM_LOW_LATENCY, media::CHANNEL_LAYOUT_MONO, 44100, 16, 2048);
-	media::AudioParameters output_params (media::AudioParameters::AUDIO_PCM_LOW_LATENCY, media::CHANNEL_LAYOUT_STEREO, 48000, 16, 2048);
+	if (!audio_manager->HasAudioOutputDevices ()) {
+		std::cout << "No audio output devices ! Exiting..." << std::endl;
+		return -1;
+	}
 
-	media::AudioHardwareConfig config (input_params, output_params);
+	 // Enumerate output devices
 
-	std::cout << "Output sample rate : " << config.GetOutputSampleRate () << std::endl;
+	media::AudioDeviceDescriptions descriptions;
+	audio_manager->GetAudioOutputDeviceDescriptions (&descriptions);
+
+	for (const auto& description : descriptions)
+		std::cout << "Audio device : " << description.device_name << std::endl; 
 
 	return 0;
 }
